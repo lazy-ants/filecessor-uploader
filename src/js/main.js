2cb769cdb68041103510f38bf4e2f02edd14380d
Coffee-Tea <bazyukvitaliy@gmail.com>
2016-04-11 11:59:38 +0300

    Fixed code style, ordered some oparations, inserted them into functions, fixed bug with reseting modal

--- src/js/main.js
+++ src/js/main.js
@@ -15,47 +15,30 @@ $(function(){
         
         formData.append('file', file);
         $.ajax({
-           url: FILECESSOR_URL + '/api/photos/upload',
-           method: 'POST',
-           data: formData,
-           contentType: false,
-           processData: false,
-           crossDomain: true,
-           success: function(data) {
-               for (var i = 0; i < data.links.length; i++) {
-                   var link = data.links[i];
-                   if (link.rel == 'original') {
-                       originLink = link.href;
-
-                       var img = $("<img>", {id: "uploaded-image", class: "uploaded-image"});
-                       var canvas = $("<canvas>", {id: "canvas-image"});
-                       $("#editor-container").append(img);
-                       $("#editor-container").append(canvas);
-
-                       img = $('#uploaded-image')[0];
-                       $('#filecessor-modal').modal();
-                       img.addEventListener("load", function() {
-                            setTimeout(function () {
-                                drawImageInCanvas(img);
-                            }, 100);
-                       }, false);
-                       $('#uploaded-image').attr("src", originLink);
-
-                       $('#close-modal')[0].addEventListener("click", function() {
-                           $( "#uploaded-image" ).remove();
-                           $( "#canvas-image" ).remove();
-                           $("#filecessor-input").get(0).value = "";
-                           $('#back-rotate-btn').off("click");
-                           $('#forward-rotate-btn').off("click");
-                       }, false);
-                   };
-               }
-           },
-           error: function () {
-           }
+            url: FILECESSOR_URL + '/api/photos/upload',
+            method: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            crossDomain: true,
+            success: function(data) {
+                for (var i = 0; i < data.links.length; i++) {
+                    var link = data.links[i];
+                    if (link.rel == 'original') {
+                        originLink = link.href;
+                        createDOMimgAndCnvs (originLink);
+                        $('.close-modal').click(function() {
+                            resetFilecessorModal();
+                        });
+                    };
+                }
+            },
+            error: function () {
+            }
         });
     });
 
+    //applying config options start _______________________________________________________________
     function applyFilecessorOptions (options) {
         !options.crop ? $('#crop-btn').css('display', 'none') : applyCropOptions(options.crop);
         !options.rotate ? $('#back-rotate-btn, #forward-rotate-btn').css('display', 'none') : applyRotateOptions(options.rotate);
@@ -91,7 +74,25 @@ $(function(){
             //TODO: apply every resize property
         }
     };
+    //applying config options end _______________________________________________________________
 
+    function createDOMimgAndCnvs (imgSrc) {
+        var img = $("<img>", {id: "uploaded-image", class: "uploaded-image"});
+        var canvas = $("<canvas>", {id: "canvas-image"});
+        $("#editor-container").append(img);
+        $("#editor-container").append(canvas);
+        img = $('#uploaded-image')[0];
+
+        $('#filecessor-modal').modal();
+        img.addEventListener("load", function() {
+            setTimeout(function () {
+                drawImageInCanvas(img);
+            }, 100);
+        }, false);
+        $('#uploaded-image').attr("src", originLink);
+    };
+
+    //operations with image start _____________________________________________________
     function drawImageInCanvas (img) {
         var canvas = $('#canvas-image')[0];
         var cContext = canvas.getContext('2d');
@@ -135,6 +136,16 @@ $(function(){
         cContext.drawImage(img, cx, cy, scaleW, scaleH);
         $('#canvas-image').attr("rotate", rotateDegree);
     }
+
+    //operations with image end _____________________________________________________
+
+    function resetFilecessorModal () {
+        $( "#uploaded-image" ).remove();
+        $( "#canvas-image" ).remove();
+        $("#filecessor-input").get(0).value = "";
+        $('#back-rotate-btn').off("click");
+        $('#forward-rotate-btn').off("click");
+    };
 
 });
 
